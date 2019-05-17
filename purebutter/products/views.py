867c870035c872df95a3805ba260d919764b0a0f
priandey <riandeypierre@gmail.com>
2019-05-17 18:34:10 +0200

    Added get_substitute view

--- purebutter/products/views.py
+++ purebutter/products/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import random
 
-# TODO : Processus de recherche de substitut
+from django.shortcuts import render
+from .models import Product
+
+def get_substitute(to_substitute, precision=10):
+    """
+
+    [param] to_substitute: Product object to substitute
+    [param] precision: Increase the amount of items taken into account in the substitution draw.
+                        Increasing precision will increase chances that a low graded product is selected.
+    [return] should return a Product object, whith a higher nutrition_grade than to_substitute
+    """
+    pot_substitutes = list()
+
+    for prod in Product.objects.order_by('nutrition_grade').filter(category=to_substitute.category):
+        if prod.nutrition_grade < to_substitute.nutrition_grade:  # 'a' is smaller than 'b'
+            pot_substitutes.append(prod)
+            precision -= 1
+
+        if precision <= 0:
+            break
+
+        return random.choice(pot_substitutes)
+
+
+# TODO : Browsing products
