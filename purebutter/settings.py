c5ef91a32ddbae951bcbaec5884658f55b190791
Priandey <riandeypierre@gmail.com>
2019-06-07 06:53:33 +0200

    setting up production

--- purebutter/settings.py
+++ purebutter/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%eoy)#0%(4$a%@s8r!hsz$bszj29ywxqtpkl4nq@da1)fhlgep'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",]
+ALLOWED_HOSTS = ["127.0.0.1", "https://pure-butter.herokuapp.com/"]
 
 SITE_ID = 1
 
