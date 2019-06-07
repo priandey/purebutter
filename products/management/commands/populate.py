3ec742359b19233469c1fad16f831ef76656f510
Priandey <riandeypierre@gmail.com>
2019-06-07 14:44:50 +0200

    back to old populate command

--- products/management/commands/populate.py
+++ products/management/commands/populate.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from products.models import Product
 
 import requests
@@ -24,43 +24,33 @@ class Command(BaseCommand):
                     'cereales-pour-petit-dejeuner',
                     'plats-prepares',
                     'fromages',
-                    'sodas',
-                    'snacks-sales'
+                    'sodas'
                     ]
 
         for category in categories:
-            openfoodfacts_response = self.get_category(category, payload)
+            self.stdout.write(f'=== RECEIVING DATA FROM OPENFOODFACTS FOR {category} ===')
+            payload['tag_1'] = category
+            raw_output = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=payload)
+            dict_output = raw_output.json()
             appended_product = list()
             product_incomplete = 0
-            for entry in openfoodfacts_response['products']:
+            for entry in dict_output['products']:
                 if entry in appended_product:
                     continue
                 else:
                     appended_product.append(entry)
                 try:
                     if entry['stores'] == '':
-                        product_incomplete += 1
                         continue
-                    self.append_product(category, entry)
+                    product = Product(name=entry['product_name'],
+                                      nutrition_grade=entry['nutrition_grades'],
+                                      url=entry['url'],
+                                      store=entry['stores'],
+                                      category=category,
+                                      thumb_link=entry["image_front_url"],
+                                      diet_link=entry['selected_images']['ingredients']['display']['fr'])
+                    product.save()
                 except KeyError:
                     product_incomplete += 1
                     continue
             self.stdout.write(f'=== DONE {product_incomplete} products incomplete ===')
-
-    @staticmethod
-    def append_product(self, category, entry):
-        product = Product(name=entry['product_name'],
-                          nutrition_grade=entry['nutrition_grades'],
-                          url=entry['url'],
-                          store=entry['stores'],
-                          category=category,
-                          thumb_link=entry["image_front_url"],
-                          diet_link=entry['selected_images']['ingredients']['display']['fr'])
-        product.save()
-
-    def get_category(self, category, payload):
-        self.stdout.write(f'=== RECEIVING DATA FROM OPENFOODFACTS FOR {category} ===')
-        payload['tag_1'] = category
-        raw_output = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=payload)
-        dict_output = raw_output.json()
-        return dict_output
