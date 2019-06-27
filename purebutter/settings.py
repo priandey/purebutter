31019a46786af980ae6f38d380e8c343919601b6
Priandey <riandeypierre@gmail.com>
2019-06-27 08:52:16 +0200

    removed heroku references in settings.py

--- purebutter/settings.py
+++ purebutter/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = "=s0%hf@)trb5)brff4j_vg+%6akoru(tjlh@a(fv^t(jqtonth"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "https://pure-butter.herokuapp.com/"]
 
@@ -131,5 +131,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AIL = True
-
-django_heroku.settings(locals())
